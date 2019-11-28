71a3450f20db02b7d57eff6d816487fcb14fc37c
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-11-27 22:39:52 -0800

    Using Variables in R
    This is a tutorial on a few methods to assign variables in R

---
+++ Using Variables.r
@@ -0,0 +1,14 @@
+# here is an ecample of how you would compute the sum for the first 20 integers
+20*(20+1)/2
+
+# however, we can define a variable to use the formula for other values of variable n
+n <- 20
+n*(n+1)/2
+
+# remember the '<-' is how you would assign values to an object 
+n <- 25
+n*(n+1)/2
+
+# below is an example that would calculate the sum of the first 100 integers
+n <- 100
+n*(n+1)/2
