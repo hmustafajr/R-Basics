d4e1eff2200cad81a971e61ea6699e42e8bd13d2
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-03 21:33:50 -0800

    Update Datasets Identical.r

--- Datasets Identical.r
+++ Datasets Identical.r
@@ -1,29 +1,29 @@
-#load package and data
+# load package and data
 
 library(dslabs)
 data(murders)
 data(movielens)
 
-#use the function names to extract the variable names
+# use the function names to extract the variable names
 names(murders)
 
-#to access the population variable from the murders dataset use the following:
+# to access the population variable from the murders dataset use the following:
 p <- murders$population
 m <- movielens$movieid
 
-#to determine the calss of object 'p' we use the following
+# to determine the calss of object 'p' we use the following
 class(p)
 class(m)
 
-#so if we change the population with murders the following should work
+# so if we change the population with murders the following should work
 x <- murders$murders
 class(x)
 
-#this is how we would extract the population:
+# this is how we would extract the population:
 p <- murders$population
 
-#this is how we would do the same with double brackets:
+# this is how we would do the same with double brackets:
 o <- murders[["population"]]
 
-#this can be confirmed by doing the following:
+# this can be confirmed by doing the following:
 identical (o, p )
