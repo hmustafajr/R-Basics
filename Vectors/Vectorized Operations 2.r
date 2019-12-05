48eac4562ecb24e0b3a8339c5e3863a64a74b29c
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-04 20:38:28 -0800

    Update Vectorized Operations 2.r

--- Vectors/Vectorized Operations 2.r
+++ Vectors/Vectorized Operations 2.r
@@ -1,134 +1,20 @@
-## Vectorized operations continued...
+## Vectorized operations continued
 
-```yaml
-type: NormalExercise
-key: 657f51ea05
-lang: r
-xp: 100
-skills: 1
-```
+# Now we will use what we have learned to perform actual calculations that would otherwise be hella complicated
 
-We can use some of what we have learned to perform calculations that 
-would otherwise be quite complicated. Let's see an example.
+# For example
 
-`@instructions`
-What is the sum of the following equation: `1 + 1/2^2 + 1/3^2 + ... + 1/100^2`? Thanks to Euler we know it should be close to $\pi^2/6$. 
+# What is the sum of the following equation:
+1 + 1/2^2 + 1/3^2 + ... 1/100^2
 
-- Define an object `x` that contains the numbers 1 through 100
-- Compute the sum `1 + 1/2^2 + 1/3^2 + ... + 1/100^2`.
+# This is similar to 
+$\pi^2/6$. 
 
-`@hint`
-Remember that you can use vectorization to get the series using `1/x^2`. You can just sum these vectors.
+# Now we will define an object that holds the values 1 to 100
+1 + 1/2^2 + 1/3^2 + ... + 1/100^2
 
-`@pre_exercise_code`
-```{r}
-# no pec 
-```
+# Remember you can use vectorization to obtain the series with 1/x^2
+# Now you just sum these vectors
 
-`@sample_code`
-```{r}
-# Define an object `x` with the numbers 1 through 100
-# Compute the sum 
-```
-
-`@solution`
-```{r}
-# Define an object `x` with the numbers 1 through 100
-x <- seq(1, 100)
-# Sum the equation 
-sum(1/x^2) 
-```
-
-`@sct`
-```{r}
-test_error()
-test_object("x", undefined_msg = "Define x first!", incorrect_msg = "Use the sequence command to store 1,100 in x.")
-test_function("sum", incorrect_msg = "Use the code given in the instructions.")
-test_output_contains("sum(1/x^2)")
-success_msg("Good job!")
-```
-
----
-
-## Vectorized operation continued...
-
-```yaml
-type: NormalExercise
-key: e5215311fc
-lang: r
-xp: 100
-skills: 1
-```
-
-
-
-`@instructions`
-- Compute the per 100,000 murder rate for each state and store it in the object `murder_rate`. 
-- Then compute the average of the state murder rates for the US using the function `mean`. What is this average?
-
-`@hint`
-You can use vectorization to compute the rate. If `tot` has the total murders and `pop` has the population for each state, then `tot/pop*100000` has the rate each state. Then you can simply compute the average using `mean`.
-
-`@pre_exercise_code`
-```{r}
-library(dslabs)
-data(murders)
-```
-
-`@sample_code`
-```{r}
-# Load the data
-library(dslabs)
-data(murders)
-# Store the per 100,000 murder rate for each state in murder_rate
-# Calculate the average murder rate in the US 
-```
-
-`@solution`
-```{r}
-# Load the data
-library(dslabs)
-data(murders)
-# Store the per 100,000 murder rate for each state in murder_rate
-murder_rate <- murders$total/murders$population*100000
-# Calculate the average murder rate in the US
-mean(murder_rate)
-```
-
-`@sct`
-```{r}
-test_error()
-test_object("murder_rate", incorrect_msg = "Your murder rate is incorrect. Make sure to multiply the result by 100,000.")
-test_function("mean", incorrect_msg = "Did you forget to take the mean of the murder rate?")
-test_output_contains("mean(murder_rate)",incorrect_msg = "Make sure to multiply the result by 100,000.")
-success_msg("Awesome!")
-```
-
----
-
-## End of Assessment 5
-
-```yaml
-type: PureMultipleChoiceExercise
-key: 9831da4a5f
-lang: r
-xp: 50
-skills: 1
-```
-
-This is the end of the programming assignment for this section. Please DO NOT click through to additional assessments from this page. Please DO answer the question on this page. If you do click through, your scores may NOT be recorded.
-
-Click on "Awesome" to get the "points" for this question and then return to the course on edX.
-
-You can now close this window to go back to the <a href='https://www.edx.org/course/data-science-r-basics-2'>course</a>.
-
-`@hint`
-- No hint necessary!
-
-`@possible_answers`
-- [Awesome]
-- Nope
-
-`@feedback`
-- Great! Now navigate back to the course on edX!
-- Now navigate back to the course on edX!
+x <- seq(1, 100)  # For example, define an object with values 1 to 100 and compute the sum
+sum(1/x^2) # Sum the equation
