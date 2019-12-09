0e6882a7113c26089c5eabd2f340d87325c20e79
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-08 18:59:04 -0800

    Create dplyr.r

---
+++ Data Wrangling/dplyr.r
@@ -0,0 +1,9 @@
+# Loading data
+library(dslabs)
+data(murders)
+
+# Loading dplyr
+library(dplyr)
+
+# Redefine murders so that it includes a column named rate with the per 100,000 murder rates
+murders <- mutate(murders, rate = total / population *100000 )
