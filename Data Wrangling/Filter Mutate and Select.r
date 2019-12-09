c856a816a48a00a32b006e3eec7bfc8d22779382
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-08 20:24:46 -0800

    State not defined error
    Used the incorrect function.

--- Data Wrangling/Filter Mutate and Select.r
+++ Data Wrangling/Filter Mutate and Select.r
@@ -5,5 +5,5 @@ data(murders)
 # Create new data frame called my_states (with specifications in the instructions)
 my_states <- murders %>%
 mutate(rate = total / population * 100000, rank = rank(-rate)) %>%
-filter(region %in% c("Northeast", "West") & rate <1) %in%
+filter(region %in% c("Northeast", "West") & rate <1) %>%
 select(state, rate, rank)
