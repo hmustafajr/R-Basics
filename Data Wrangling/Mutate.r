bb4e238233d4e7f40df49125be06df2d9c477b5a
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-08 19:01:01 -0800

    Create Mutate.r

---
+++ Data Wrangling/Mutate.r
@@ -0,0 +1,11 @@
+# Note that if you want ranks from highest to lowest you can take the negative and then compute the ranks 
+x <- c(88, 100, 83, 92, 94)
+rank(-x)
+
+# Defining rate
+rate <-  murders$total/ murders$population * 100000
+
+# Redefine murders to include a column named rank
+# with the ranks of rate from highest to lowest
+
+murders <- mutate(murders, rank = rank(-rate))
