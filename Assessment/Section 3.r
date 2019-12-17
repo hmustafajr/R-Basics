c981c2699feb6870e1fc57dc5a339da657e6fb15
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-17 00:30:49 -0800

    Update Section 3.r

--- Assessment/Section 3.r
+++ Assessment/Section 3.r
@@ -20,3 +20,20 @@ sum (x)
 
 v <- heights$sex =="Female"&heights$height >68.3
 sum(v)
+#############
+
+library(dslabs)
+data(heights)
+options(digits = 3) #report 3 significant digits for all answers
+
+x <- heights$height
+y <- mean(x)
+
+x <- c(heights$height>68.3)
+sum (x)
+
+v <- heights$sex =="Female"&heights$height >68.3
+sum(v)
+
+z <- heights$sex =="Female"
+mean(z)
