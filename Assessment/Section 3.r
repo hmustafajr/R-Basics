5fbe7024eb87a5c18edc548745394287044a34a9
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-17 20:08:21 -0800

    Update Section 3.r

--- Assessment/Section 3.r
+++ Assessment/Section 3.r
@@ -4,39 +4,26 @@ options(digits = 3) #report 3 significant digits for all answers
 
 x <- heights$height
 y <- mean(x)
-
 x <- c(heights$height>68.3)
 
-###
-library(dslabs)
-data(heights)
-options(digits = 3) #report 3 significant digits for all answers
+#
 
 x <- heights$height
 y <- mean(x)
-
 x <- c(heights$height>68.3)
 sum (x)
-
 v <- heights$sex =="Female"&heights$height >68.3
 sum(v)
-#############
 
-library(dslabs)
-data(heights)
-options(digits = 3) #report 3 significant digits for all answers
+#
 
 x <- heights$height
 y <- mean(x)
-
 x <- c(heights$height>68.3)
 sum (x)
-
 v <- heights$sex =="Female"&heights$height >68.3
 sum(v)
-
 z <- heights$sex =="Female"
 mean(z)
-
 y <- which.min(heights$height)
 heights$height[y]
