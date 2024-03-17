37d4296123bc4f89c4f2dc3d1a0b4e228e91b8bc
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-03-17 11:19:04 -0700

    Update Sort test.r

--- Sorting/Sort test.r
+++ Sorting/Sort test.r
@@ -1,36 +1,33 @@
-# accesses the 'state' variable and stores it in an object called 'state'
+# accesses the 'state' variable and stores the 'state' object
 states <- murder$state
 
-# sorts the object alphabetically and redefines the object
+# alphabetically sorts and redefines the object
 states <- sort(states)
 
-# reports the first value alphabetically
+# displays the first value alphabetically
 states[1]
 
-# the following is a test created to replicate for 'population'
-
+# below is a test created to replicate for 'population'
 # accesses population values from the dataset and stores it in pop
 pop <- murders$population
 
-# sorts the object and saves it in the same object
+# sorts and saves the object
 pop <- sort(pop)
 
-# reports the smallest population by size
+# displays the smallest population by size
 pop[1]
-
-# not too sure on how to explain this, I just started typing and it works
 
 # accesses the population from the dataset and stores it in pop
 pop <- murders$population
 
-# use the command order to identify the vector of indices and places pop in an order and then stores it in the object ord
+# use the order command to identify the vector of indices, sorts pop in an order, and stores it in the object ord
 ord <- order(pop)
 
-# finds the index number of the entry with the smallest population size
+# identifies the index number of the entry with the smallest population size
 ord[1]
 
-# finds the index of the smallest value for variable total
+# identifies the index of the smallest value for variable total
 which.min(murders$total)
 
-# finds the index of the smallest value for population
+# identifies the index of the smallest value for population
 which.min(population)
