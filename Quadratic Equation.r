d2a255a0160241c065cb1eb2c0c5961798ae840d
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-03-17 10:36:07 -0700

    Update Quadratic Equation.r

--- Quadratic Equation.r
+++ Quadratic Equation.r
@@ -1,16 +1,16 @@
-# assigning values to variables
+# assigns values to variables
 a <- 1
 b <- 1
 c <- -1
 
-# test 1 solving the quadratic equation
+# test case 1 to solve the quadratic equation
 (-b + sqrt(b^2 - 4*a*c) / (2*a))
 (-b - sqrt(b^2 - 4*a*c) / (2*a))
 
-# test 2 using the correct formula
+# test case 2 using the correct formula
 (-b + sqrt(b^2 - 4*a*c)) / (2*a)
 (-b - sqrt(b^2 - 4*a*c)) / (2*a)
 
-# assisgning solutions
+# assisgnin the solutions
 solution1 <- (-b + sqrt(b^2 - 4*a*c)) / (2*a)
 solution2 <- (-b - sqrt(b^2 - 4*a*c)) / (2*a)
