ffbfd18a9dbf34b9de9f887cee835274bc52b1de
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-03-23 10:46:55 -0700

    Update Sequential Sum.r

--- Variables/Sequential Sum.r
+++ Variables/Sequential Sum.r
@@ -1,8 +1,11 @@
-n <- 1000 # assigns the value of one thousand to n
-+ x <- seq(1,n) # the above is how a sequence is created
-+ sum(x) # this is a list of numbers and sum adds them together
+# assigns the value of one thousand to n
+n <- 1000
+# below is how a sequence is created
++ x <- seq(1,n)
+# this is a list of numbers and sum adds them together
++ sum(x)
 
-# what I've in math and more in programming is that we say we evaluate a function when arguments are replaced specific values
+# what I've learned in math and more in programming is that we say we evaluate a function when arguments replace specific values
 # for example if we type log2(16) we evaluate the log2 function to get the log base 2 of 16 which will return 4
 log2(16)
 
@@ -11,6 +14,5 @@ log2(16)
 sqrt(log2(16))
 
 # so the first evaluation returns 4 and this is then evaluated by sqrt to give the final answer of 2
-
-x <-5 # 5 is a random number that I chose for testing
+x <-5 # 5 is a random number I chose to test
 log(exp(x)) # this will always return the numeric value stored in x
