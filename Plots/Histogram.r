426b3bd6cb005087eea880a60941d5286b834bf2
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-03-23 10:24:26 -0700

    Update Histogram.r

--- Plots/Histogram.r
+++ Plots/Histogram.r
@@ -1,5 +1,5 @@
-# Store the population in millions and save to population_in_millions 
+# store the population in millions and saves to population_in_millions 
 population_in_millions <- murders$population/10^6
 
-# Create a histogram of this variable
+# creates a histogram of this variable
 hist(population_in_millions)
