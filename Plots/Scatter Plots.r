2fedff7516c6f48b006b70b9b13a5f889b6e2e1b
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-03-23 10:27:51 -0700

    Update Scatter Plots.r

--- Plots/Scatter Plots.r
+++ Plots/Scatter Plots.r
@@ -1,4 +1,4 @@
-# Load the datasets and define some variables
+# load the datasets and define some variables
 library(dslabs)
 data(murders)
 
@@ -7,11 +7,11 @@ total_gun_murders <- murders$total
 
 plot(population_in_millions, total_gun_murders)
 
-# Transform population using the log10 transformation and save to object log10_population
+# transform population using the log10 transformation and save to object log10_population
 log10_population <- log10(murders$population)
 
-# Transform total gun murders using log10 transformation and save to object log10_total_gun_murders
+# transform total gun murders using log10 transformation and save to object log10_total_gun_murders
 log10_total_gun_murders <- log10(murders$total)
 
-# Create a scatterplot with the log scale transformed population and murders 
+# create a scatterplot with the log scale transformed population and murders 
 plot(log10_population, log10_total_gun_murders)
